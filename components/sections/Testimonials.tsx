eaa9b260d97d54cfb233e424ebb924a2e04f6e13
tj <thinupavani@gmail.com>
2023-06-14 13:45:06 +0530

    update

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -1,5 +1,5 @@
-import { useContext, useState } from "react";
-import { BsArrowRight } from 'react-icons/bs'
+import { useContext, useEffect, useState } from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 import {
   Navigation,
@@ -8,7 +8,7 @@ import {
   A11y,
   Autoplay,
   EffectFade,
-  Mousewheel
+  Mousewheel,
 } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -20,7 +20,7 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/effect-fade";
 import "swiper/css/mousewheel";
-import Image from 'next/image';
+import Image from "next/image";
 
 // theme styles
 import lightStyles from "@/styles//Light.module.css";
@@ -31,84 +31,100 @@ const Testimonials = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [activeButtonIndex, setActiveButtonIndex] = useState(0);
 
+  useEffect(() => {
+    
+  }, [activeButtonIndex])
+  
   const { theme } = useContext(ThemeContext);
 
   const testimonialsData = [
     {
       id: 1,
-      imgPath: '/imgs/testimonials/clients (1).png',
+      imgPath: "/imgs/testimonials/clients (1).png",
       desc: `I tell you what, there has never been an issue that Cory from the Colabrio Support Team hasn't been able to solve for me.`,
-      name:'Alex Regelman',
-      position:'Co-founder, Colabrio'
+      name: "Alex Regelman",
+      position: "Co-founder, Colabrio",
     },
     {
       id: 2,
-      imgPath: '/imgs/testimonials/clients (2).png',
+      imgPath: "/imgs/testimonials/clients (2).png",
       desc: `I tell you what, there has never been an issue that Cory from the Colabrio Support Team hasn't been able to solve for me.`,
-      name:'Laura Morton',
-      position:'UI/UX Designer, Colabrio'
+      name: "Laura Morton",
+      position: "UI/UX Designer, Colabrio",
     },
     {
       id: 3,
-      imgPath: '/imgs/testimonials/clients (3).jpg',
+      imgPath: "/imgs/testimonials/clients (3).jpg",
       desc: `I tell you what, there has never been an issue that Cory from the Colabrio Support Team hasn't been able to solve for me.`,
-      name:'Stacey Rickson',
-      position:'Project Manager, Colabrio'
-    }
-  ]
+      name: "Stacey Rickson",
+      position: "Project Manager, Colabrio",
+    },
+  ];
 
-  
   const sliderStyles =
     theme === "dark" ? darkStyles.sliderbg : lightStyles.sliderbg;
   return (
     <div className="d-flex flex-column flex-lg-row left-right-space py-5 w-100 testimonials-styles">
       <div className="col-12 col-lg-6">
         <h3>Testimonials</h3>
-        <h2 className='mb-4'>What our clients say <br /> about our studio.</h2>
+        <h2 className="mb-4">
+          What our clients say <br /> about our studio.
+        </h2>
         <button type="submit" className="conatct-btn px-3 py-2">
-            More Reviews <BsArrowRight />
-          </button>
+          More Reviews <BsArrowRight />
+        </button>
       </div>
       {/* slider */}
-      <div className="col-12 col-lg-6">
-      <Swiper
-      id='testimonialsSlider'
-          modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Mousewheel]}
+      <div className="col-12 col-lg-6 position-relative">
+        <Swiper
+          id="testimonialsSlider"
+          modules={[
+            Navigation,
+            Pagination,
+            Scrollbar,
+            A11y,
+            EffectFade,
+            Mousewheel,
+          ]}
           spaceBetween={0}
           slidesPerView={1}
           effect="fade"
           speed={500}
           loop={true}
-          // mousewheel
-          // scrollbar={{ draggable: true,  }}
-          // pagination={{ clickable: true }}
           navigation={true}
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={(swiper) => {
-            console.log(swiper.activeIndex)
-            setActiveButtonIndex(swiper.activeIndex)
+            console.log(swiper.activeIndex);
+            setActiveButtonIndex(swiper.activeIndex);
           }}
         >
           {testimonialsData.map((item) => (
             <SwiperSlide key={item.id}>
-              <div className={`d-flex testimonial-slider-style flex-column px-5 py-5 ${sliderStyles}`}>
-              <Image
-              src={item.imgPath}
-              alt=""
-              width={100}
-              height={100}
-              className="img-fluid mb-3 round-image"
-            />
-            <p>{item.desc}</p>
-            <h2>{item.name}</h2>
-            <h3>{item.position}</h3>
+              <div
+                className={`d-flex testimonial-slider-style flex-column px-5 py-5 ${sliderStyles}`}
+              >
+                <Image
+                  src={item.imgPath}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className="img-fluid mb-3 round-image"
+                />
+                <p>{item.desc}</p>
+                <h2>{item.name}</h2>
+                <h3>{item.position}</h3>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
+        <div className="d-flex flex-row align-items-center px-5">
+          <p className="mb-0">{activeButtonIndex}</p>
+          <div className="slideline mx-2"></div>
+          <p className="mb-0 ">{testimonialsData.length}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
